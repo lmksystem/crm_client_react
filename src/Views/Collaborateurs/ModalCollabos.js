59dee0c2ea57c3199ddbe5025ceef6523d3ee1dd
jordan <jordan@lmksystem.fr>
2024-11-27 09:53:52 +0100

    debut list col

--- src/Views/Collaborateurs/ModalCollabos.js
+++ src/Views/Collaborateurs/ModalCollabos.js
@@ -1,17 +1,48 @@
-import React from "react";
-import { Input, Modal, ModalBody, ModalFooter, ModalHeader, Row } from "reactstrap";
+import React, { useEffect, useMemo, useState } from "react";
+import {
+  Input,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  Row,
+} from "reactstrap";
 import SimpleBar from "simplebar-react";
 
-function ModalCollabos() {
+function ModalCollabos({ modalClient, toggleModalClient }) {
+  const [searchValueClient, setSearchValueClient] = useState("");
+  const [collaborateurs, setCollaborateurs] = useState([]);
+
+  const listClient = useMemo(() => {
+    let data = [...collaborateurs];
+    if (searchValueClient != "") {
+      data = data.filter(
+        (e) =>
+          e.ent_name
+            ?.toLowerCase()
+            ?.includes(searchValueClient.toLowerCase()) ||
+          e.ent_email
+            ?.toLowerCase()
+            ?.includes(searchValueClient.toLowerCase()) ||
+          e.ent_phone?.toLowerCase()?.includes(searchValueClient.toLowerCase())
+      );
+    }
+
+    return data;
+  }, [searchValueClient]);
+
+  useEffect(() => {
+    return () => {};
+  }, []);
+
   return (
     <Modal
       id="showModal"
       isOpen={modalClient}
       toggle={toggleModalClient}
-      centered>
-      <ModalHeader
-        className="bg-soft-info p-3"
-        toggle={toggleModalClient}>
+      centered
+    >
+      <ModalHeader className="bg-soft-info p-3" toggle={toggleModalClient}>
         Sélectionnez un client
       </ModalHeader>
 
@@ -30,41 +61,67 @@ function ModalCollabos() {
           <SimpleBar
             autoHide={false}
             style={{ maxHeight: "220px" }}
-            className="px-3">
-            {handleListClient()?.map((c, i) => {
+            className="px-3"
+          >
+            {listClient?.map((c, i) => {
               return (
                 <div
                   key={i}
-                  style={{ display: "flex", alignItems: "center", width: "100%", borderBottom: "0.5px solid #dddddd", margin: 3 }}>
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    width: "100%",
+                    borderBottom: "0.5px solid #dddddd",
+                    margin: 3,
+                  }}
+                >
                   <div className="flex-shrink-0">
                     {c.ent_img_url ? (
                       <img
-                        src={process.env.REACT_APP_API_URL + "/images/" + c.ent_img_url}
+                        src={
+                          process.env.REACT_APP_API_URL +
+                          "/images/" +
+                          c.ent_img_url
+                        }
                         alt=""
                         className="avatar-xxs rounded-circle"
                       />
                     ) : (
                       <div className="flex-shrink-0 avatar-xs me-2">
-                        <div className="avatar-title bg-soft-success text-success rounded-circle fs-13">{c.ent_name.charAt(0)}</div>
+                        <div className="avatar-title bg-soft-success text-success rounded-circle fs-13">
+                          {c.ent_name.charAt(0)}
+                        </div>
                       </div>
                     )}
                   </div>
                   <div
-                    style={{ cursor: "pointer", padding: 8, display: "flex", flexDirection: "column", width: "100%" }}
+                    style={{
+                      cursor: "pointer",
+                      padding: 8,
+                      display: "flex",
+                      flexDirection: "column",
+                      width: "100%",
+                    }}
                     onClick={() => {
-                      setModalClient(() => false);
+                      toggleModalClient();
                       validation.setValues({
                         ...validation.values,
                         recurrence_data: {
                           ...validation.values.recurrence_data,
-                          rec_ent_fk: c.ent_id
+                          rec_ent_fk: c.ent_id,
                         },
-                        clients: c
+                        clients: c,
                       });
                     }}
-                    key={i}>
+                    key={i}
+                  >
                     <span>{c.ent_name}</span>
-                    <div style={{ display: "flex", justifyContent: "space-between" }}>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                      }}
+                    >
                       <span>
                         <i>{c.ent_email}</i>
                       </span>{" "}
@@ -85,15 +142,13 @@ function ModalCollabos() {
             type="button"
             className="btn btn-light"
             onClick={() => {
-              setModalClient(false);
-            }}>
+              toggleModalClient();
+            }}
+          >
             {" "}
             Fermer{" "}
           </button>
-          <button
-            type="submit"
-            className="btn btn-success"
-            id="add-btn">
+          <button type="submit" className="btn btn-success" id="add-btn">
             {" "}
             Sélectionner{" "}
           </button>
